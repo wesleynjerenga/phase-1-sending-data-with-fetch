c3b12fcf1013a6ef3cfab90ce36e3e970840bf93
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-19 20:30:19 +0300

    sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+let fullName ="";
+let email ="";
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newId = document.createElement('p');
+        newId.textContent = data.id;
+        document.body.appendChild(newId);
+    })
+    .catch(error => {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+    });
+}
